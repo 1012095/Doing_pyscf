432f66ba344e9e4e209c7ed8f42fff64658eb9f8
1012095 <1012095@users.noreply.github.com>
2025-06-19 06:21:34 +0700

    create a new init file so ours

---
+++ mp/seft_develop_files/__init__.py
@@ -0,0 +1,26 @@
+# OUR TEAM UNIVERSITY OF SCIENCE SEFT DEVELOP
+# Copyright 2014-2020 The PySCF Developers. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from pyscf import scf
+from pyscf.mp import mp2
+from pyscf.mp import dfmp2
+from pyscf.mp import ump2
+from pyscf.mp import gmp2
+from pyscf.mp import dfgmp2
+from pyscf.mp import obmp2, obmp2_active, obmp2_faster
+from pyscf.mp import uobmp2, uobmp2_dfold, uobmp2_faster
+from pyscf.mp import uobmp2_active, uobmp2_active_scf
+from pyscf.mp import dfobmp2_faster_ram, dfuobmp2_faster_ram, uobmp2_mom_conv
+from pyscf.mp import ub2plyp_dfuobmp2_diis
